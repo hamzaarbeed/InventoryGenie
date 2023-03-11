a6be2269ccd7edd7e78e3e8eab09cdbbb930a89f
hamzaarbeed <hamza.arbeed@gmail.com>
2023-03-11 01:06:13 -0600

    added supplier tab in navigation bar

---
+++ InventoryGenie/Controllers/SupplierController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryGenie.Controllers
+{
+    public class SupplierController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
